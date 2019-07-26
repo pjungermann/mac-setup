c3898a4977d2d2512fcaa28647262e67434607a4
Patrick Jungermann <patrick.jungermann@gmail.com>
2019-07-26 21:31:48 +0200

    install or upgrade for brew (cask)

--- mac-setup.sh
+++ mac-setup.sh
@@ -20,6 +20,48 @@ function user_confirms() {
   fi
 }
 
+function brew_install_or_upgrade() {
+  formulae=$1
+  prompt_upgrade=$2
+  prompt_install=$3
+  
+  if [ -z "$(brew list | grep "$formulae")" ]
+  then
+    if [ -z "$prompt_install" ] || user_confirms "$prompt_install"
+    then
+      brew install "$formulae"
+      echo "installed $formulae"
+    fi
+  else
+    if [ -z "$prompt_upgrade" ] || user_confirms "$prompt_upgrade"
+    then
+      brew upgrade "$formulae"
+      echo "upgraded $formulae"
+    fi
+  fi
+}
+
+function brew_cask_install_or_upgrade() {
+  formulae=$1
+  prompt_upgrade=$2
+  prompt_install=$3
+  
+  if [ -z "$(brew list | grep "$formulae")" ]
+  then
+    if [ -z "$prompt_install" ] || user_confirms "$prompt_install"
+    then
+      brew cask install "$formulae"
+      echo "installed $formulae"
+    fi
+  else
+    if [ -z "$prompt_upgrade" ] || user_confirms "$prompt_upgrade"
+    then
+      brew cask upgrade "$formulae"
+      echo "upgraded $formulae"
+    fi
+  fi
+}
+
 # 1. install xcode Command Line Tools (CLT)
 if xcode-select -p 2> /dev/null 
 then
@@ -54,39 +96,30 @@ echo "installed oh-my-zsh"
 echo "installed homebrew with commands: 'brew' and 'brew cask'"
 
 # 3.2. install "mas" Mac App Store CLI
-brew install mas
+brew_install_or_upgrade 'mas' 'Upgrade MAS (Mac App Store CLI)?'
 
 # 3.3. install python and "pip" (python package manager)
-brew install python
+brew_install_or_upgrade 'python' 'Upgrade Python?'
 
 # 3.4. install "SDK Man" (SDK manager for SKDs like Java, Groovy, Grails, Maven, Gradle, ...)
 curl -s "https://get.sdkman.io" | bash
 echo "installed SDK Man"
 
 # 4. install CLI tools
-brew install coreutils
-echo "installed coreutils"
-
-brew install htop
-echo "installed command htop"
-
-brew install tldr
-echo "installed command tldr"
+brew_install_or_upgrade 'coreutils' 'Upgrade coreutils?'
+brew_install_or_upgrade 'htop'
+brew_install_or_upgrade 'tldr'
 
 # 5. infrastructure tools
 # 5.1. install fabric
-if user_confirms "install fabric?"
+if user_confirms "Install fabric?"
 then
   pip3 install fabric
   echo "installed fabric"
 fi
 
 # 5.2. install terraform
-if user_confirms "install terraform?"
-then
-  brew install terraform
-  echo "installed terraform"
-fi
+brew_install_or_upgrade 'terraform' 'Upgrade terraform?' 'Install terraform?'
 
 # 5.3. install awscli
 if user_confirms "install AWS CLI?"
@@ -96,27 +129,12 @@ then
 fi
 
 # 5.4. install container service
-brew cask install docker
+brew_cask_install_or_upgrade 'docker' 'Upgrade Docker?'
 echo "installed the latest version of: Docker"
 
 # 5.5. install kubernetes-related tools
-if [ -z "$(brew list | grep kubernetes-cli)" ]
-then
-  if user_confirms "install kubernetes-cli?"
-  then
-    rm /usr/local/bin/kubectl # kubectl by Docker installation; also available as kubectl.docker
-    brew install kubernetes-cli
-    echo "installed kubernetes-cli"
-  fi
-fi
-if [ -z "$(brew list | grep kubernetes-helm)" ]
-then
-  if user_confirms "install kubernetes-helm?"
-  then
-    brew install kubernetes-helm
-    echo "installed kubernetes-helm"
-  fi
-fi
+brew_install_or_upgrade 'kubernetes-cli' 'Upgrade kubernetes-cli?' 'Install kubernetes-cli?'
+brew_install_or_upgrade 'kubernetes-helm' 'Upgrade kubernetes-helm?' 'Install kubernetes-helm?'
 
 # 6. install SDKs
 zsh -i -c 'sdk install java $(sdk list java | grep zulu | head -n 1 | cut -d "|" -f 6 | sed -E -e "s#[^0-9]*(.*zulu).*#\1#g")'
@@ -127,23 +145,14 @@ zsh -i -c 'sdk install gradle'
 echo "installed the latest SDKs for: Java, Groovy, Kotlin, Maven, Gradle"
 
 # 7. install basic software
-if user_confirms "install Tunnelblick (OpenVPN)?"
-then
-  brew cask install tunnelblick
-  echo "installed the lastest version of Tunnelblick (VPN)"
-fi
+brew_cask_install_or_upgrade 'tunnelblick' 'Upgrade Tunnelblick (OpenVPN)?' 'Install Tunnelblick (OpenVPN)?'
 
 # 8. install (BE) developer software
-brew cask install pgadmin4
-brew cask install intellij-idea
-echo "installed the latest version of: pgAdmin4, IntelliJ IDEA"
+brew_cask_install_or_uprgade 'pgadmin4' 'Upgrade pgAdmin4?'
+brew_cask_install_or_upgrade 'intellij-idea' 'Upgrade IntelliJ IDEA?'
 
 # 9. install communication software
-if user_confirms "install Skype?"
-then
-  brew cask install skype
-  echo "installed the lastest version of Skype"
-fi
+brew_cask_install_or_upgrade 'skype' 'Upgrade Skype?' 'Install Skype?'
 if user_confirms "install Slack?"
 then
   slack_id=$(mas search Slack | grep -E -e "\s+\d+\s+Slack" | sed -E -e "s/[^0-9]*([0-9]+).*/\1/g")
@@ -175,11 +184,7 @@ else
 fi
 
 # 11. Akamai CLI
-if user_confirms "install Akamai CLI?"
-then
-  brew install akamai
-  echo "installed Akamai CLI"
-fi
+brew_install_or_upgrade 'akamai' 'Upgrade Akamai CLI?' 'Install Akamai CLI??'
 
 
 echo "finished with the machine setup"
