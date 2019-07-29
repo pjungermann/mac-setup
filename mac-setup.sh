a50fddd9aa0fb919719281aeb055c23ddad75a73
Patrick Jungermann <Patrick.Jungermann@gmail.com>
2019-07-29 18:56:41 +0200

    simplify brew install functions

--- mac-setup.sh
+++ mac-setup.sh
@@ -23,43 +23,101 @@ function user_confirms() {
   fi
 }
 
-function brew_install_or_upgrade() {
-  formulae=$1
-  prompt_upgrade=$2
-  prompt_install=$3
-  
-  if [[ -z "$(brew list | grep "$formulae")" ]]
+function with_brew() {
+  formulae="$1"
+  force_upgrade=true
+  force_install=true
+  display_name=""
+
+  ## parse arguments
+  # arg 2
+  if [[ "$2" = "false" ]]
   then
-    if [[ -z "$prompt_install" ]] || user_confirms "$prompt_install"
+    force_upgrade=false
+
+  elif [[ "$2" != "true" ]]
+  then
+    display_name="$2"
+  fi
+  # arg 3
+  if [[ -z "$display_name" ]] && [[ "$3" = "false" ]]
+  then
+    force_install=false
+
+  elif [[ -z "$display_name" ]] && [[ "$3" != "true" ]]
+  then
+    display_name="$3"
+  fi
+  # arg 4
+  if [[ -z "$display_name" ]] && [[ -n "$4" ]]
+  then
+    display_name="$4"
+  else
+    display_name="$formulae"
+  fi
+
+  ## execute install/upgrade
+  if [[ -z "$(brew list | grep "$formulae")" ]]
+  then # not installed, yet
+    if ${force_install} || user_confirms "Install $display_name?"
     then
       brew install "$formulae"
       echo "installed $formulae"
     fi
-  else
-    if [[ -z "$prompt_upgrade" ]] || user_confirms "$prompt_upgrade"
+  else # already installed. Upgrade?
+    if ${force_upgrade} || user_confirms "Upgrade $display_name?"
     then
       brew upgrade "$formulae" && echo "upgraded $formulae" || echo "not upgraded"
     fi
   fi
+
 }
 
-function brew_cask_install_or_upgrade() {
-  formulae=$1
-  prompt_upgrade=$2
-  prompt_install=$3
-  
-  if [[ -z "$(brew list | grep "$formulae")" ]]
+function with_brew_cask() {
+  formulae="$1"
+  force_upgrade=true
+  force_install=true
+  display_name=""
+
+  ## parse arguments
+  # arg 2
+  if [[ "$2" = "false" ]]
   then
-    if [[ -z "$prompt_install" ]] || user_confirms "$prompt_install"
+    force_upgrade=false
+
+  elif [[ "$2" != "true" ]]
+  then
+    display_name="$2"
+  fi
+  # arg 3
+  if [[ -z "$display_name" ]] && [[ "$3" = "false" ]]
+  then
+    force_install=false
+
+  elif [[ -z "$display_name" ]] && [[ "$3" != "true" ]]
+  then
+    display_name="$3"
+  fi
+  # arg 4
+  if [[ -z "$display_name" ]] && [[ -n "$4" ]]
+  then
+    display_name="$4"
+  else
+    display_name="$formulae"
+  fi
+
+  ## execute install/upgrade
+  if [[ -z "$(brew list | grep "$formulae")" ]]
+  then # not installed, yet
+    if ${force_install} || user_confirms "Install $display_name?"
     then
-      brew cask install --force "$formulae"
+      brew cask install "$formulae"
       echo "installed $formulae"
     fi
-  else
-    if [[ -z "$prompt_upgrade" ]] || user_confirms "$prompt_upgrade"
+  else # already installed. Upgrade?
+    if ${force_upgrade} || user_confirms "Upgrade $display_name?"
     then
-      brew cask upgrade "$formulae"
-      echo "upgraded $formulae"
+      brew cask upgrade "$formulae" && echo "upgraded $formulae" || echo "not upgraded"
     fi
   fi
 }
@@ -133,11 +191,11 @@ else
 fi
 
 # 3.2. install "mas" Mac App Store CLI
-brew_install_or_upgrade 'mas' 'Upgrade MAS (Mac App Store CLI)?'
+with_brew 'mas' false 'MAS (Mac App Store CLI)'
 
 # 3.3. install python and "pip" (python package manager), conda
-brew_install_or_upgrade 'python' 'Upgrade Python?'
-brew_cask_install_or_upgrade 'miniconda' 'Upgrade miniconda?' 'Install miniconda?'
+with_brew 'python' false
+with_brew_cask 'miniconda' false false
 
 # 3.4. install "SDKMAN!" (SDK manager for SDKs like Java, Groovy, Kotlin, Maven, Gradle, ...)
 if ! zsh -i -c 'which sdk &> /dev/null' || [[ ! -d "${user_home}/.sdkman" ]]
@@ -147,14 +205,14 @@ then
 fi
 
 # 4. install CLI tools
-brew_install_or_upgrade 'coreutils' 'Upgrade coreutils?'
-brew_install_or_upgrade 'htop'
-brew_install_or_upgrade 'nmap'
-brew_install_or_upgrade 'parallel'
-brew_install_or_upgrade 'tldr'
-brew_install_or_upgrade 'watch'
-brew_install_or_upgrade 'wifi-password' 'Upgrade wifi-password?' 'Install wifi-password?'
-brew_install_or_upgrade 'speedtest-cli' 'Upgrade speedtest-cli?' 'Install speedtest-cli?'
+with_brew 'coreutils' false
+with_brew 'htop'
+with_brew 'nmap'
+with_brew 'parallel'
+with_brew 'tldr'
+with_brew 'watch'
+with_brew 'wifi-password' false false
+with_brew 'speedtest-cli' false false
 
 # 5. infrastructure tools
 # 5.1. install fabric
@@ -165,7 +223,7 @@ then
 fi
 
 # 5.2. install terraform
-brew_install_or_upgrade 'terraform' 'Upgrade terraform?' 'Install terraform?'
+with_brew 'terraform' false false
 
 # 5.3. install awscli
 if user_confirms "install AWS CLI?"
@@ -175,14 +233,13 @@ then
 fi
 
 # 5.4. install container service
-brew_cask_install_or_upgrade 'docker' 'Upgrade Docker?'
-echo "installed the latest version of: Docker"
+with_brew_cask 'docker' false
 
 # 5.5. install kubernetes-related tools
 rm -f /usr/local/bin/kubectl
-brew_install_or_upgrade 'kubernetes-cli' 'Upgrade kubernetes-cli?'
-brew_install_or_upgrade 'kubernetes-helm' 'Upgrade kubernetes-helm?' 'Install kubernetes-helm?'
-brew_cask_install_or_upgrade 'minikube' 'Upgrade minikube?' 'Install minikube?'
+with_brew 'kubernetes-cli' false
+with_brew 'kubernetes-helm' false false
+with_brew_cask 'minikube' false false
 
 # 6. install SDKs
 zsh -i -c 'sdk install java $(sdk list java | grep zulu | head -n 1 | cut -d "|" -f 6 | sed -E -e "s#[^0-9]*(.*zulu).*#\1#g")'
@@ -193,58 +250,58 @@ zsh -i -c 'sdk install gradle'
 echo "installed the latest SDKs for: Java, Groovy, Kotlin, Maven, Gradle"
 
 # 7. install basic software
-brew_cask_install_or_upgrade 'tunnelblick' 'Upgrade Tunnelblick (OpenVPN)?' 'Install Tunnelblick (OpenVPN)?'
+with_brew_cask 'tunnelblick' false false 'Tunnelblick (OpenVPN)'
 
 # 8. install communication software
-brew_cask_install_or_upgrade 'skype' 'Upgrade Skype?' 'Install Skype?'
-brew_cask_install_or_upgrade 'skype-for-business' 'Upgrade Skype for Business?' 'Install Skype for Business?'
+with_brew_cask 'skype' false false 'Skype'
+with_brew_cask 'skype-for-business' false false 'Skype for Business'
 mas_install 'Slack'
 
 # 9. install developer software
 # 9.1. Terminal alternatives
-brew_cask_install_or_upgrade 'iterm2' 'Upgrade iTerm2?' 'Install iTerm2?'
+with_brew_cask 'iterm2' false false 'iTerm2'
 
 # 9.2. DB
-brew_cask_install_or_upgrade 'pgadmin4' 'Upgrade pgAdmin4?'
+with_brew_cask 'pgadmin4' false 'pgAdmin4'
 
 # 9.3. IDEs etc.
-brew_cask_install_or_upgrade 'jetbrains-toolbox' 'Upgrade Jetbrains Toolbox?'
-brew_cask_install_or_upgrade 'intellij-idea' 'Upgrade IntelliJ IDEA?'
-brew_cask_install_or_upgrade 'visual-studio-code' 'Upgrade VS Code?' 'Install VS Code?'
+with_brew_cask 'jetbrains-toolbox' false 'Jetbrains Toolbox'
+with_brew_cask 'intellij-idea' false 'IntelliJ IDEA'
+with_brew_cask 'visual-studio-code' false 'VS Code'
 
 # 9.4. APIs
-brew_cask_install_or_upgrade 'insomnia' 'Upgrade Insomnia?' 'Install Insomnia?'
-brew_cask_install_or_upgrade 'postman' 'Upgrade Postman?' 'Install Postman?'
+with_brew_cask 'insomnia' false false 'Insomnia'
+with_brew_cask 'postman' false false 'Postman'
 
 # 10. browsers
-brew_cask_install_or_upgrade 'firefox' 'Upgrade Firefox?' 'Install Firefox?'
+with_brew_cask 'firefox' false false 'Firefox'
 if [[ ! -d "${user_home}/Applications/Chrome Apps.localized" ]]
 then
-  brew_cask_install_or_upgrade 'google-chrome' 'Upgrade Google Chrome?' 'Install Google Chrome?'
+  with_brew_cask 'google-chrome' false false 'Google Chrome'
 else
   echo "Google Chrome is already installed"
 fi
 
 # 11. Akamai CLI
-brew_install_or_upgrade 'akamai' 'Upgrade Akamai CLI?' 'Install Akamai CLI??'
+with_brew 'akamai' false false 'Akamai CLI'
 
 # 12. Airtame
-brew_cask_install_or_upgrade 'airtame' 'Upgrade Airtame?' 'Install Airtame?'
+with_brew_cask 'airtame' false false 'Airtame'
 
 # 13. Cyberduck
-brew_cask_install_or_upgrade 'cyberduck' 'Upgrade Cyberduck?' 'Install Cyberduck?'
+with_brew_cask 'cyberduck' false false 'Cyberduck'
 
 # 14. Balsamiq Mockup
-brew_cask_install_or_upgrade 'balsamiq-mockups' 'Upgrade Balamiq Mockups?' 'Install Balsamiq Mockups?'
+with_brew_cask 'balsamiq-mockups' false false 'Balamiq Mockups'
 
 # 15. Camunda Modeler
-brew_cask_install_or_upgrade 'camunda-modeler' 'Upgrade Camunda Modeler?' 'Install Camunda Modeler?'
+with_brew_cask 'camunda-modeler' false false 'Camunda Modeler'
 
 # 16. Zeebe Modeler
-brew_cask_install_or_upgrade 'zeebe-modeler' 'Upgrade Zeebe Modeler?' 'Install Zeebe Modeler?'
+with_brew_cask 'zeebe-modeler' false false 'Zeebe Modeler'
 
 # 17. Dropbox
-brew_cask_install_or_upgrade 'dropbox' 'Upgrade Dropbox?' 'Install Dropbox?'
+with_brew_cask 'dropbox' false false 'Dropbox'
 
 # 18. Amphetamine
 mas_install 'Amphetamine'
