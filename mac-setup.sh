0b8c29107be7d4f6d35d89518758bb2ae2fc9b7e
Patrick Jungermann <Patrick.Jungermann@gmail.com>
2021-06-10 16:48:16 +0200

    chore: make compatible with latest Homebrew version
    Homebrew removed the `brew cask` command in favor of
    unifying the commands for formulae and casks while
    providing options to specify which of these you want
    (e.g., using `--formula` or `--cask`).
    
    Additionally, `brew info --json=v2` is a new version of
    the JSON-based info command which now includes support for
    casks, too.
    
    This allowed for unifying the functions used for both cases.
    
    Naming collisions between formulae (homebrew/core) and casks (homebrew/cask)
    can exist and in these cases, Homebrew will pick the formula and print a warning.
    It is possible to use a qualified identifier to point to a specific one.
    
    This was necessary i.e. for "docker" as we have the cask for the Docker CE desktop app
    as well as the CLI(-only) formula.
    
    A conflict at the upgrade procedure at miniconda
    required to change the order and install/upgrade Python first.
    
    Fixes issue #8.

--- mac-setup.sh
+++ mac-setup.sh
@@ -33,40 +33,12 @@ user_confirms () {
 brew_installed () {
   formula="$1"
 
-  brew list | grep -q -E "^${formula}$"
-}
-
-brew_cask_installed () {
-  formula="$1"
-
-  brew cask list | grep -q -E "^${formula}$"
-}
-
-brew_current_version () {
-  formula="$1"
-
-  # handle brew and brew cask
-  # cask: redirection error output and filter error message header
-  version="$(
-    brew info "$formula" 2>&1 \
-     | grep -e '/Cellar/' \
-            -e '/Caskroom/' \
-     | sed -E -e 's#.*/([^ ]+).*#\1#'
-  )"
-
-  echo "$version"
-}
-
-brew_latest_version () {
-  formula="$1"
-
-  # revision info is not available at the non-json output
-  # and current version / version folder uses it as suffix for >0 cases
-  #
-  # revision == 0: {versions.stable}
-  # revision  > 0: {versions.stable}_{revision}
-  brew info --json "$formula" \
-    | jq --raw-output '"\(.[0].versions.stable)\(.[0].revision | if . <= 0 then "" else "_\(.)" end)"'
+  if ! which jq >/dev/null; then
+    brew list | grep -E -e "^${formula}$" >/dev/null
+  else
+    brew info --json=v2 "$formula" \
+      | jq -e '(.formulae[0].installed | length > 0) or (.casks[0].installed | length > 0)' >/dev/null
+  fi
 }
 
 is_outdated_brew () {
@@ -78,18 +50,8 @@ is_outdated_brew () {
     return # maybe. Not really expected though
   fi
 
-  current="$(brew_current_version "$formula")"
-  latest="$(brew_latest_version "$formula")"
-
-  [[ "$current" != "$latest" ]]
-}
-
-is_outdated_brew_cask () {
-  formula="$1"
-
-  # --greedy: includes casks with version "latest" and "auto-update: true"
-  # --quiet : suppress version information; only show the formula
-  brew cask outdated --greedy --quiet | grep -q -E -e "^${formula}$"
+  brew info --json=v2 "$formula" \
+    | jq -e '.formulae[0].outdated or .casks[0].outdated' >/dev/null
 }
 
 with_brew () {
@@ -138,57 +100,6 @@ with_brew () {
     if ${force_upgrade} || user_confirms "Upgrade $display_name?"
     then
       brew upgrade "$formula" && echo "upgraded $formula" || echo "not upgraded"
-    fi
-  fi
-}
-
-with_brew_cask () {
-  formula="$1"
-  force_upgrade=true
-  force_install=true
-  display_name=""
-
-  ## parse arguments
-  # arg 2
-  if [[ "$2" = "false" ]]
-  then
-    force_upgrade=false
-
-  elif [[ "$2" != "true" ]]
-  then
-    display_name="$2"
-  fi
-  # arg 3
-  if [[ -z "$display_name" ]] && [[ "$3" = "false" ]]
-  then
-    force_install=false
-
-  elif [[ -z "$display_name" ]] && [[ "$3" != "true" ]]
-  then
-    display_name="$3"
-  fi
-  # arg 4
-  if [[ -z "$display_name" ]] && [[ -n "$4" ]]
-  then
-    display_name="$4"
-  else
-    display_name="$formula"
-  fi
-
-  ## execute install/upgrade
-  if ! brew_cask_installed "$formula"
-  then # not installed, yet
-    if ${force_install} || user_confirms "Install $display_name?"
-    then
-      # option "--force" will overwrite manual installed versions
-      brew cask install --force "$formula"
-      echo "installed $formula"
-    fi
-  elif is_outdated_brew_cask "$formula"
-  then # already installed, but not latest. Upgrade?
-    if ${force_upgrade} || user_confirms "Upgrade $display_name?"
-    then
-      brew cask upgrade "$formula" && echo "upgraded $formula" || echo "not upgraded"
     fi
   fi
 }
@@ -273,11 +184,11 @@ with_brew 'mas' false 'MAS (Mac App Store CLI)'
 mas upgrade
 
 # 3.3. install python and "pip" (python package manager), conda
-# 3.3.1. (mini)conda (with own python and pip)
-with_brew_cask 'miniconda' false false
-conda init "$(basename "${SHELL}")"
-# 3.3.2. latest python
+# 3.3.1. latest python
 with_brew 'python' false
+# 3.3.2. (mini)conda (with own python and pip)
+with_brew 'miniconda' false false
+conda init "$(basename "${SHELL}")"
 ## make unversioned commands point to the latest
 # shellcheck disable=SC2016
 zshrc_append='export PATH="/usr/local/opt/python/libexec/bin:${PATH}"'
@@ -347,14 +258,14 @@ then
 fi
 
 # 5.4. install container service
-with_brew_cask 'docker' false
+with_brew 'homebrew/cask/docker' false
 with_brew 'container-diff'
 
 # 5.5. install kubernetes-related tools
 rm -f /usr/local/bin/kubectl
 with_brew 'kubernetes-cli' false
 with_brew 'kubernetes-helm' false false
-with_brew_cask 'minikube' false false
+with_brew 'minikube' false false
 
 # 6. install SDKs
 zsh -i -c 'sdk install java $(sdk list java | grep zulu | head -n 1 | cut -d "|" -f 6 | sed -E -e "s#[^0-9]*(.*zulu).*#\1#g")'
@@ -365,61 +276,61 @@ zsh -i -c 'sdk install gradle'
 echo "installed the latest SDKs for: Java, Groovy, Kotlin, Maven, Gradle"
 
 # 7. install basic software
-with_brew_cask 'tunnelblick' false false 'Tunnelblick (OpenVPN)'
+with_brew 'tunnelblick' false false 'Tunnelblick (OpenVPN)'
 
 # 8. install communication software
-with_brew_cask 'skype' false false 'Skype'  # TODO: drop?
+with_brew 'skype' false false 'Skype'  # TODO: drop?
 mas_install 'Slack'
 
 # 9. install developer software
 # 9.1. Terminal alternatives
-with_brew_cask 'iterm2' false false 'iTerm2'
+with_brew 'iterm2' false false 'iTerm2'
 
 # 9.2. DB
-with_brew_cask 'pgadmin4' false 'pgAdmin4'
+with_brew 'pgadmin4' false 'pgAdmin4'
 
 # 9.3. IDEs etc.
-with_brew_cask 'jetbrains-toolbox' false 'Jetbrains Toolbox'
-with_brew_cask 'intellij-idea' false 'IntelliJ IDEA'
-with_brew_cask 'visual-studio-code' false false 'VS Code'
+with_brew 'jetbrains-toolbox' false 'Jetbrains Toolbox'
+with_brew 'intellij-idea' false 'IntelliJ IDEA'
+with_brew 'visual-studio-code' false false 'VS Code'
 
 # 9.4. APIs
-with_brew_cask 'insomnia' false false 'Insomnia'
-with_brew_cask 'postman' false false 'Postman'
+with_brew 'insomnia' false false 'Insomnia'
+with_brew 'postman' false false 'Postman'
 
 # 10. browsers
-with_brew_cask 'firefox' false false 'Firefox'
-with_brew_cask 'google-chrome' false false 'Google Chrome'
+with_brew 'firefox' false false 'Firefox'
+with_brew 'google-chrome' false false 'Google Chrome'
 
 # 11. Akamai CLI
 with_brew 'akamai' false false 'Akamai CLI'
 
 # 12. Airtame
-with_brew_cask 'airtame' false false 'Airtame'
+with_brew 'airtame' false false 'Airtame'
 
 # 13. Cyberduck
-with_brew_cask 'cyberduck' false false 'Cyberduck'
+with_brew 'cyberduck' false false 'Cyberduck'
 
 # 14. Balsamiq Mockup
-with_brew_cask 'balsamiq-mockups' false false 'Balamiq Mockups'
+with_brew 'balsamiq-mockups' false false 'Balamiq Mockups'
 
 # 15. Camunda Modeler
-with_brew_cask 'camunda-modeler' false false 'Camunda Modeler'
+with_brew 'camunda-modeler' false false 'Camunda Modeler'
 
 # 16. Zeebe Modeler
-with_brew_cask 'zeebe-modeler' false false 'Zeebe Modeler'
+with_brew 'zeebe-modeler' false false 'Zeebe Modeler'
 
 # 17. Dropbox
-with_brew_cask 'dropbox' false false 'Dropbox'
+with_brew 'dropbox' false false 'Dropbox'
 
 # 18. Amphetamine
 mas_install 'Amphetamine'
 
 # 19. Window Management
-with_brew_cask 'spectacle' false false 'Spectacle'
+with_brew 'spectacle' false false 'Spectacle'
 
 # 20. Music
-with_brew_cask 'spotify' false false 'Spotify'
+with_brew 'spotify' false false 'Spotify'
 
 # 21. Oh My Zsh - Plugins
 zsh_plugins="\
